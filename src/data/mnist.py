155beba287dcf431a2ac5a47d64186aa652ec761
k-kuroguro <lingjiujinshan@gmail.com>
2024-12-29 04:11:46 +0900

    wip

--- src/data/mnist.py
+++ src/data/mnist.py
@@ -55,6 +55,7 @@ class MNISTDataModule(LightningDataModule):
     def val_dataloader(self) -> DataLoader[Any]:
         return DataLoader(
             dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
             shuffle=False,
--- src/train.py
+++ src/train.py
@@ -7,27 +7,39 @@ from omegaconf import DictConfig, OmegaConf
 
 from utils import get_config_path, register_custom_resolvers
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 register_custom_resolvers()
 
 
 def train(cfg: DictConfig) -> None:
+    log.info(f"Instantiating {cfg.model._target_} as model")
     model = instantiate(cfg.model)
     datamodule = instantiate(cfg.data)
     if cfg.get("logger", None):
-        logger = instantiate(cfg.logger)
-        logger.log_hyperparams(OmegaConf.to_container(cfg, resolve=True))
+        loggers = [instantiate(c) for c in cfg.logger.values()]
+        for logger in loggers:
+            logger.log_hyperparams(OmegaConf.to_container(cfg, resolve=True))
+        logger = loggers
+        print(loggers)
     else:
         logger = False
-    trainer = instantiate(cfg.trainer, callbacks=None, logger=logger)
+    callbacks = [instantiate(c) for c in cfg.callbacks.values()]
+    trainer = instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
     trainer.fit(model, datamodule)
     # trainer.test()
+    if logger:
+        for logger in loggers:
+            if isinstance(logger, L.pytorch.loggers.WandbLogger):
+                import wandb
+
+                if wandb.run:
+                    wandb.finish()
 
 
 @hydra.main(version_base="1.3", config_path=str(get_config_path()), config_name="train.yaml")
 def main(cfg: DictConfig) -> None:
-    logger.info("Start training")
+    log.info("Start training")
     OmegaConf.resolve(cfg)
     print(OmegaConf.to_yaml(cfg))
     train(cfg)
